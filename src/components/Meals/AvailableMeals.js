826ad54bfa06c20454844e40332b4788c8f3fbdc
Aditya9699 <70836785+Aditya9699@users.noreply.github.com>
2023-07-18 00:35:14 +0530

    Update AvailableMeals.js

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/meals.json'
+        'https://food-data-9918c-default-rtdb.firebaseio.com/Meals.json'
       );
 
       if (!response.ok) {
